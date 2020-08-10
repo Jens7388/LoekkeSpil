54dc4631188757311fdbe2722149037211f571a8
Jens Zakarias Pedersen <jens7388@edu.campusvejle.dk>
2020-08-10 09:41:00 +0200

    Add project files.

---
+++ LoekkeSpil/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LoekkeSpil
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
