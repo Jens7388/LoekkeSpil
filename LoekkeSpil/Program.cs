3ae732152f312c63d94e2da02e027913de7c3c92
Jens Zakarias Pedersen <jens7388@edu.campusvejle.dk>
2020-08-10 11:14:11 +0200

    Added player class

--- LoekkeSpil/Program.cs
+++ LoekkeSpil/Program.cs
@@ -4,54 +4,76 @@ namespace LoekkeSpil
 {
     class Program
     {
-        private int noOfPlayers;
-        private int currentPlayer;
-        private int points;
-        private void rollDice()
+        static int noOfPlayers;
+        static int currentPlayer;
+        static int points;
+
+        private static void rollDice()
         {
             Random dice = new Random();
             int diceRoll = dice.Next(1, 7);
             if(diceRoll == 1)
             {
                 Console.WriteLine("Du slog 1!, Du mister din tur, og alle dine point..");
+                points = 0;
+                Console.ReadLine();
+                passTurn();         
             }
             else
             {
-
+                Console.WriteLine($"Du slog: {diceRoll}");
+                points += diceRoll;
             }
-            Console.WriteLine(diceRoll);
-            Console.ReadLine();
         }
-        private void passTurn()
+
+        private static void passTurn()
         {
             if(currentPlayer < noOfPlayers)
             {
                 currentPlayer++;
                 Console.Clear();
-                Console.WriteLine($"Spiller {currentPlayer}'s tur, tryk y for at slå med terningen, tryk n for at sende turen videre.");
-            }
-        }
-
-        void Main()
-        {
-            Console.WriteLine("Hvor mange spillere er med?");
-            int noOfPlayers;
-            int.TryParse(Console.ReadLine(), out noOfPlayers);
-            currentPlayer = 1;
-            Console.WriteLine($"Spiller {currentPlayer}'s tur, tryk y for at slå med terningen, tryk n for at sende turen videre.");
-            string input = Console.ReadLine();
-            if(input.ToLower() == "y")
-            {
-                rollDice();
-            }
-            else if(input.ToLower() == "n")
-            {
-                passTurn();
+                turn();
             }
             else
             {
-                Console.WriteLine("Ugyldigt input! Prøv igen");
+                currentPlayer = 1;
+                Console.Clear();
+                turn();
             }
+        }
+
+        private static void turn()
+        {
+            Console.WriteLine($"Spiller {currentPlayer}'s tur, tryk y for at slå med terningen, tryk n for at sende turen videre.");        
+            while(true)
+            {
+                string input = Console.ReadLine();
+                if(input.ToLower() == "y")
+                {
+                    rollDice();
+                }
+                else if(input.ToLower() == "n")
+                {
+                    Console.WriteLine($"Du har scoret {points} i denne runde");
+                    Console.ReadLine();
+                    passTurn();
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Ugyldigt input! Prøv igen");
+                    break;
+                }
+            }
+            
+        }
+
+        static void Main()
+        {
+            Console.WriteLine("Hvor mange spillere er med?");
+            int.TryParse(Console.ReadLine(), out noOfPlayers);
+            currentPlayer = 1;
+            turn();
         }
     }
 }
---
+++ LoekkeSpil/player.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LoekkeSpil
+{
+    class player
+    {
+        protected string name;
+        protected int points;
+
+        public player(string name, int points)
+        {
+            Name = name;
+            Points = points;
+        }
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+        public int Points
+        {
+            get
+            {
+                return points;
+            }
+            set
+            {
+                points = value;
+            }
+        }
+    }
+}
