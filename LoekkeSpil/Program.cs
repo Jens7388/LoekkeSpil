e40f749875ea4348f90af9fcaa27ca23f5c9e0a5
Jens Zakarias Pedersen <jens7388@edu.campusvejle.dk>
2020-08-10 13:38:42 +0200

    Capitalized method names

--- LoekkeSpil/Program.cs
+++ LoekkeSpil/Program.cs
@@ -10,12 +10,11 @@ namespace LoekkeSpil
         static int roundPoints;
         static List<Player> players;
 
-        private static void createPlayers()
+        private static void CreatePlayers()
         {
             Console.Clear();
             Console.WriteLine("Hvor mange spillere er med?");
-            int noOfPlayers;
-            int.TryParse(Console.ReadLine(), out noOfPlayers);
+            int.TryParse(Console.ReadLine(), out int noOfPlayers);
             players = new List<Player>();
             for(int i = 1; i <= noOfPlayers; i++)
             {
@@ -27,7 +26,7 @@ namespace LoekkeSpil
             currentPlayer = 0;
         }
 
-        private static void displayScoreBoard()
+        private static void DisplayScoreBoard()
         {
             Console.WriteLine("Point:");
             for(int i = 0; i < players.Count; i++)
@@ -36,7 +35,7 @@ namespace LoekkeSpil
             }
         }
 
-        private static void rollDice()
+        private static void RollDice()
         {
             Random dice = new Random();
             int diceRoll = dice.Next(1, 7);
@@ -45,7 +44,7 @@ namespace LoekkeSpil
                 Console.WriteLine("\nDu slog 1!, Du mister din tur, og alle dine point..");
                 players[currentPlayer].Points = 0;
                 Console.ReadLine();
-                passTurn();         
+                PassTurn();         
             }
             else
             {
@@ -55,32 +54,32 @@ namespace LoekkeSpil
             }
         }
 
-        private static void passTurn()
+        private static void PassTurn()
         {
             roundPoints = 0;
             if(currentPlayer < players.Count -1)
             {
                 currentPlayer++;
-                turn();
+                Turn();
             }
             else
             {
                 currentPlayer = 0;
-                turn();
+                Turn();
             }
         }
 
-        private static void turn()
+        private static void Turn()
         {
             Console.Clear();
-            displayScoreBoard();
+            DisplayScoreBoard();
             Console.WriteLine($"\n{players[currentPlayer].Name}'s tur, tryk y for at slå med terningen, tryk n for at sende turen videre.");        
             while(true)
             {
                 string input = Console.ReadKey().Key.ToString();
                 if(input.ToLower() == "y")
                 {
-                    rollDice();
+                    RollDice();
                 }
                 else if(input.ToLower() == "n")
                 {
@@ -100,7 +99,7 @@ namespace LoekkeSpil
                         break;
                     }
                     Console.ReadLine();
-                    passTurn();
+                    PassTurn();
                     break;
                 }
                 else
@@ -112,8 +111,8 @@ namespace LoekkeSpil
 
         static void Main()
         {
-            createPlayers();
-            turn();
+            CreatePlayers();
+            Turn();
         }
     }
 }
