8ab6a33d8ad55fc72ba661fcf7862273e6f377e7
Jens Zakarias Pedersen <jens7388@edu.campusvejle.dk>
2020-08-10 10:25:16 +0200

    Removed String args

--- LoekkeSpil/Program.cs
+++ LoekkeSpil/Program.cs
@@ -4,7 +4,7 @@ namespace LoekkeSpil
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             Console.WriteLine("Hello World!");
         }
